86080e0b170ff913d7d221bdd684502752fe11ea
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-01-24 20:31:40 +0530

    added Block and Transaction Model & Migrations

---
+++ database/migrations/2024_01_24_144346_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Block;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('uploaded_file_path');
+            $table->string('file_uploaded_by');
+            $table->string('file_stored_by');
+            $table->foreignIdFor(Block::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,48 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+
+
+// function DivideIntoChunks($file,$buffer=1024){
+//     //open file to read
+//     $file_handle = fopen($file,'r');
+//     //get file size
+//     $file_size = filesize($file);
+//     //no of parts to split
+//     $parts = $file_size / $buffer;
+
+//     //store all the file names
+//     $file_parts = array();
+
+//     //path to write the final files
+//     $store_path = "splits/";
+
+//     //name of input file
+//     $file_name = basename($file);
+
+//     for($i=0;$i<$parts;$i++){
+//         //read buffer sized amount from file
+//         $file_part = fread($file_handle, $buffer);
+//         //the filename of the part
+//         $file_part_path = $store_path.$file_name.".part$i";
+//         //open the new file [create it] to write
+//         $file_new = fopen($file_part_path,'w+');
+//         //write the part of file
+//         // Storage::disk('public')
+//         fwrite($file_new, $file_part);
+//         //add the name of the file to part list [optional]
+//         array_push($file_parts, $file_part_path);
+//         //close the part file handle
+//         fclose($file_new);
+//     }    
+//     //close the main file handle
+
+//     fclose($file_handle);
+//     return $file_parts;
+// }
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +57,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::post('/', function (Request $request) {
+    $file = $request->file('file');
+    $file->store('/tests', 'public');
+    // return $file->getContent();
+    return Hash::make($file->getContent());
+
 });
