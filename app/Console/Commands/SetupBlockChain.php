100c1bf12638b2a01e09e6a1806a26c488e5d1cd
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-03-10 23:34:58 +0530

    updated setup blockchain

--- app/Console/Commands/SetupBlockChain.php
+++ app/Console/Commands/SetupBlockChain.php
@@ -3,8 +3,12 @@
 namespace App\Console\Commands;
 
 use App\Models\Block;
+use App\Models\Transaction;
+use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Validation\ValidationException;
 
 class SetupBlockChain extends Command
 {
@@ -30,12 +34,24 @@ class SetupBlockChain extends Command
         $this->call('db:wipe');
         $this->call('migrate');
         $response = Http::get(env('OTHER_CSP_URL', 'http://127.0.0.1:8080') . '/api/full_blockchain');
-        $blocks = json_decode($response->body());
 
-        foreach ($blocks as $block) {
-            Block::create($block);
-        }
+        // $this->info($blocks);
+        DB::transaction(function () use ($response) {
+            $blocks = json_decode($response->body(), true);
+            foreach ($blocks as $block) {
+                $blockModel = Block::create([
+                    'id' => $block['id'],
+                    'previous_block_hash' => $block['previous_block_hash'],
+                ]);
+                foreach ($block['transactions'] as $transaction) {
+                    if (!Transaction::verify($transaction)) {
+                        throw new Exception("transaction with id {$transaction['id']} is invalid");
+                    }
+                }
 
+                $blockModel->Transactions()->createMany($block['transactions']);
+            }
+        });
 
     }
 }
